0d7f93f604f70d940076701acfa448ad73501bc4
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-27 23:47:30 +0530

    [Harman] ADD:Software Engineer File

---
+++ src/com/bridgeLabz/ResumeScanning/SoftwareEngineer.java
@@ -0,0 +1,11 @@
+package com.bridgeLabz.ResumeScanning;
+class SoftwareEngineer extends JobRole {
+    public SoftwareEngineer() {
+        super("Software Engineer");
+    }
+
+    @Override
+    public void jobDescription() {
+        System.out.println("Responsible for developing and maintaining software applications.");
+    }
+}
