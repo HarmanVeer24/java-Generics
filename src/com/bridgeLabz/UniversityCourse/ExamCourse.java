b778231737c7524a35edf6b80aa4f6e494c1e3d6
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-27 23:26:21 +0530

    [Harman] ADD: ExamCourse File

---
+++ src/com/bridgeLabz/UniversityCourse/ExamCourse.java
@@ -0,0 +1,15 @@
+package com.bridgeLabz.UniversityCourse;
+
+public class ExamCourse extends CourseType {
+    private int totalMarks;
+
+    public ExamCourse(String courseName, String department, int totalMarks) {
+        super(courseName, department);
+        this.totalMarks = totalMarks;
+    }
+
+    @Override
+    public void displayCourseDetails() {
+        System.out.println("Exam-Based Course: " + getCourseName() + " | Department: " + getDepartment() + " | Total Marks: " + totalMarks);
+    }
+}
