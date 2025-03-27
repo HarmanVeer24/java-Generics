bdba00405cb9a9203fcc3d741e38cf87055f28cb
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-27 23:24:14 +0530

    [Harman] ADD:Course File

---
+++ src/com/bridgeLabz/UniversityCourse/Course.java
@@ -0,0 +1,15 @@
+package com.bridgeLabz.UniversityCourse;
+public class Course<T extends CourseType> {
+    private T courseType;
+    private String instructor;
+
+    public Course(T courseType, String instructor) {
+        this.courseType = courseType;
+        this.instructor = instructor;
+    }
+
+    public void displayCourseInfo() {
+        courseType.displayCourseDetails();
+        System.out.println("Instructor: " + instructor);
+    }
+}
