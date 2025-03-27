e52928128f5e2cae0fb7d5af61316d5ba6c61186
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-27 23:35:50 +0530

    [Harman] ADD:Keto Meal File

---
+++ src/com/bridgeLabz/MealPlan/KetoMeal.java
@@ -0,0 +1,14 @@
+package com.bridgeLabz.MealPlan;
+
+public class KetoMeal implements MealPlan {
+    private String mealName;
+
+    public KetoMeal(String mealName) {
+        this.mealName = mealName;
+    }
+
+    @Override
+    public void showMealDetails() {
+        System.out.println("Keto Meal: " + mealName);
+    }
+}
