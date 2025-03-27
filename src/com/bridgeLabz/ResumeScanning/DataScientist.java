3a7749bf5dcc620e9078eee085286aa93b5c831a
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-27 23:46:11 +0530

    [Harman] ADD:DataScientist File

---
+++ src/com/bridgeLabz/ResumeScanning/DataScientist.java
@@ -0,0 +1,12 @@
+package com.bridgeLabz.ResumeScanning;
+
+public class DataScientist extends JobRole {
+    public DataScientist() {
+        super("Data Scientist");
+    }
+
+    @Override
+    public void jobDescription() {
+        System.out.println("Responsible for analyzing and interpreting complex data to support decision-making.");
+    }
+}
