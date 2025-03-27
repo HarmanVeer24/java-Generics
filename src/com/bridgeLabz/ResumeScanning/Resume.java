840283892693062877d23d0810711685885de3eb
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-27 23:46:58 +0530

    [Harman] ADD:Resume File

---
+++ src/com/bridgeLabz/ResumeScanning/Resume.java
@@ -0,0 +1,17 @@
+package com.bridgeLabz.ResumeScanning;
+
+public class Resume<T extends JobRole> {
+    private String candidateName;
+    private T jobRole;
+
+    public Resume(String candidateName, T jobRole) {
+        this.candidateName = candidateName;
+        this.jobRole = jobRole;
+    }
+
+    public void displayResumeDetails() {
+        System.out.println("Candidate: " + candidateName);
+        System.out.println("Applied for: " + jobRole.getJobTitle());
+        jobRole.jobDescription();
+    }
+}
