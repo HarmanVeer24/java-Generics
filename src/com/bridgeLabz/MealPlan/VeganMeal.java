4490e62ebc2e0331f39dd8465eb6b9b02b9daa5b
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-27 23:36:12 +0530

    [Harman] ADD:Vegan Meal File

---
+++ src/com/bridgeLabz/MealPlan/VeganMeal.java
@@ -0,0 +1,14 @@
+package com.bridgeLabz.MealPlan;
+
+public class VeganMeal implements MealPlan {
+    private String mealName;
+
+    public VeganMeal(String mealName) {
+        this.mealName = mealName;
+    }
+
+    @Override
+    public void showMealDetails() {
+        System.out.println("Vegan Meal: " + mealName);
+    }
+}
