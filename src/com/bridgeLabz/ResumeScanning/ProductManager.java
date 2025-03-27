17b20438692806090f6a43403b89704e406bb360
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-27 23:46:37 +0530

    [Harman] ADD:ProductManager File

---
+++ src/com/bridgeLabz/ResumeScanning/ProductManager.java
@@ -0,0 +1,12 @@
+package com.bridgeLabz.ResumeScanning;
+
+public class ProductManager extends JobRole {
+    public ProductManager() {
+        super("Product Manager");
+    }
+
+    @Override
+    public void jobDescription() {
+        System.out.println("Oversees product development and strategy.");
+    }
+}
